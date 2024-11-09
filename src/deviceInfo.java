413174929e515096734a25c0f8f001c104c1d8c0
Himanshu <77005352+astatiner@users.noreply.github.com>
2024-11-09 10:42:28 +0530

    Update deviceInfo.java
    Added functionality for Linux and Mac

--- src/deviceInfo.java
+++ src/deviceInfo.java
@@ -1,26 +1,36 @@
 import java.io.*;
 import java.util.*;
+
 public class deviceInfo {
     static String systemDrive;
-    static String deviceNameInfo, osInfo, fileSeperator,basePath;
+    static String deviceNameInfo, osInfo, fileSeparator, basePath;
     static int algodata;
-    public static void relInfo() throws Exception{
+
+    public static void relInfo() throws Exception {
         deviceNameInfo = System.getProperty("user.name");
-        osInfo = System.getProperty("os.name");
-        fileSeperator = System.getProperty("file.seperator");
-        if(osInfo.contains("Windows")){
+        osInfo = System.getProperty("os.name").toLowerCase();
+        fileSeparator = System.getProperty("file.separator");
+
+        if (osInfo.contains("windows")) {
             systemDrive = System.getenv("SystemDrive");
+            basePath = systemDrive + fileSeparator + "Users" + fileSeparator + deviceNameInfo + fileSeparator + "EncyphrVault";
+        } else if (osInfo.contains("linux") || osInfo.contains("unix") || osInfo.contains("mac")) { // Added support for Linux, Unix, and Mac
+            systemDrive = System.getProperty("user.home"); // Use user's home directory as base
+            basePath = systemDrive + fileSeparator + ".EncyphrVault"; // Use a hidden directory (prefix with '.')x
+        } else {
+            throw new UnsupportedOperationException("Unsupported Operating System: " + osInfo);
         }
-        basePath=systemDrive+"\\users\\"+deviceNameInfo+"\\EncyphrVault";
-        File file = new File(basePath+"\\cryptdata.eph");
+
+        File file = new File(basePath + fileSeparator + "cryptdata.eph");
         ArrayList<String> md = new ArrayList<>();
-        if(file.exists()){
-            BufferedReader reader = new BufferedReader(new FileReader(file.getAbsolutePath()));
-            String currentLine;
-            while((currentLine = reader.readLine()) != null){
-                md.add(currentLine);
+        if (file.exists()) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(file.getAbsolutePath()))) {
+                String currentLine;
+                while ((currentLine = reader.readLine()) != null) {
+                    md.add(currentLine);
+                }
+                algodata = Integer.parseInt(md.get(0));
             }
-            algodata = Integer.parseInt(md.get(0));
         }
     }
 }
